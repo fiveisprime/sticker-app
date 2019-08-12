4d2cc683263a251124c5bc11bbfd96520e1bf218
Matt Hernandez <maherna@microsoft.com>
2019-08-12 09:22:19 -0700

    Add application insights support via the extension.
    Includes the SDK and automatic import code generated from the extension.

--- server/index.js
+++ server/index.js
@@ -1,3 +1,8 @@
+
+// Add your instrumentation key or use the APPLICATIONINSIGHTSKEY environment variable on your production machine to start collecting data.
+var ai = require('applicationinsights');
+ai.setup(process.env.APPLICATIONINSIGHTSKEY || 'your_instrumentation_key').start();
+
 const path = require('path')
 const express = require('express')
 const https = require('https')
